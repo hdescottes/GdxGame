14a1021095bb834b96d2e81df699565795566bac
HugoD <hudescottes@hotmail.fr>
2021-08-14 14:54:40 +0200

    Add battleUI tests

---
+++ core/src/test/java/com/gdx/game/battle/BattleUITest.java
@@ -0,0 +1,37 @@
+package com.gdx.game.battle;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.gdx.game.GdxRunner;
+import com.gdx.game.manager.ResourceManager;
+import com.gdx.game.profile.ProfileManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(GdxRunner.class)
+public class BattleUITest {
+
+    @BeforeEach
+    void init() {
+        Gdx.gl = mock(GL20.class);
+        Gdx.gl20 = mock(GL20.class);
+        new ResourceManager();
+    }
+
+    @Test
+    public void testBattleUI_ShouldSucceed() {
+        ProfileManager profileManager = ProfileManager.getInstance();
+        profileManager.setProperty("currentPlayerAP", 5);
+        profileManager.setProperty("currentPlayerDP", 5);
+
+        BattleState battleState = new BattleState();
+        BattleUI battleUI = new BattleUI(battleState);
+
+        assertThat(battleUI).isNotNull();
+        assertThat(battleUI.getChildren()).hasSize(2);
+    }
+}
