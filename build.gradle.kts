cadc46989feea1e8d9134230bcae475e6d1ffa25
hudescottes <hudescottes@hotmail.fr>
2022-09-18 19:58:29 +0200

    Upgrade libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 val gdxVersion by extra {"1.11.0"}
 val gdxVfxVersion by extra {"0.5.0"}
-val jUnitPlatformVersion by extra {"1.8.2"}
-val jUnitJupiterVersion by extra {"5.8.2"}
-val mockitoVersion by extra {"4.5.1"}
-val assertJVersion by extra {"3.22.0"}
-val logbackVersion by extra {"1.3.0-alpha14"}
+val jUnitPlatformVersion by extra {"1.9.0"}
+val jUnitJupiterVersion by extra {"5.9.0"}
+val mockitoVersion by extra {"4.8.0"}
+val assertJVersion by extra {"3.23.1"}
+val logbackVersion by extra {"1.4.1"}
 
 plugins {
     java
